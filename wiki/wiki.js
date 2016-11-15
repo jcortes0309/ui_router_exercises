02cd8b3bd7ee5322da0dfd05f9ab70925261b524
Juan A Cortes <juan.cortes@gmail.com>
2016-11-15 11:38:03 -0500

    Step 3: Create an edit page that allows you to update a Wiki page

--- wiki/wiki.js
+++ wiki/wiki.js
@@ -15,12 +15,30 @@ app.controller("HomeController", function($scope) {
   // controller code
 });
 
-app.controller("PageViewController", function($scope, $stateParams) {
+app.controller("PageViewController", function($scope, $stateParams, $state) {
+  $scope.pageName = $stateParams.page_name;
+  console.log("$scope.pageName is: ", $scope.pageName);
+  $scope.page = pages[$scope.pageName];
+  console.log("$scope.page is: ", $scope.page);
 
+  $scope.edit = function() {
+    $state.go("page_edit", { page_name: $scope.pageName } );
+    console.log($scope.pageName);
+  };
+});
+
+app.controller("PageEditController", function($scope, $stateParams, $state) {
   $scope.pageName = $stateParams.page_name;
   console.log($scope.pageName);
   $scope.page = pages[$scope.pageName];
+
   console.log($scope.page);
+
+  $scope.done = function() {
+    pages[$scope.pageName].content = $scope.page.content;
+    console.log("Clicked the done button");
+    $state.go("page_view", { page_name: $scope.pageName });
+  };
 });
 
 // app.controller("PagesController", function($scope, $stateParams) {
@@ -43,6 +61,12 @@ app.config(function($stateProvider, $urlRouterProvider) {
       url: "/{page_name}",
       templateUrl: "page_view.html",
       controller: "PageViewController"
+    })
+    .state({
+      name: "page_edit",
+      url: "/{page_name}/edit",
+      templateUrl: "page_edit.html",
+      controller: "PageEditController"
     })
     .state({
       name: "pages",
